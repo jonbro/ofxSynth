af5606cd6a8e3fff0ae1dc375050b7b4d0dfdf08
jonbro <jonbro@gmail.com>
2011-03-03 16:23:22 -0500

    forking the ofxSynth for more focused work

---
+++ src/ofxSynthDelayLine.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "ofSoundEffect.h"
+
+class ofxSynthDelayline : public ofSoundEffect {
+	public:
+		
+		ofxSynthDelayline();
+		string getName() { return "ofxSynthDelayLine"; }
+		virtual void process( float* input, float *output, int numFrames, int numInChannels, int numOutChannels );
+		void setSize(float _size);
+		void setFeedback(float _feedback);
+		void setMix(float _mix);
+		
+	private:
+		float frequency;
+		int phase;
+		float startphase;
+		float endphase;
+		float output;
+		float memory[88200];
+		int size;
+		float feedback;
+		float mix;
+};
